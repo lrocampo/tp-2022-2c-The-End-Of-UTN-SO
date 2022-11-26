67ab33ec55f40c3950013fc52c485e5f85fb26e3
valentinarau <vraurobayo@frba.utn.edu.ar>
2022-11-26 01:48:03 +0000

    binding de ips, a testear

---
+++ config_ip.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+function main () {
+    if (($# != 3)); then
+        echo -e "\e[1;91mParameters: <kernelIP> <memoriaIP> <cpuIP>\e[0m"
+        exit 1
+    fi
+
+    local -r kernelIP=$1
+    local -r memoriaIP=$2
+    local -r cpuIP=$3
+
+    # Cambiar IP de memoria y cpu en kernel
+    perl -pi -e "s/(?<=IP_MEMORIA=).*/${memoriaIP}/g" kernel/configs/*
+    perl -pi -e "s/(?<=IP_CPU=).*/${cpuIP}/g" kernel/cfg/*
+
+    # Cambiar IP de memoria en cpu
+    perl -pi -e "s/(?<=IP_MEMORIA=).*/${memoriaIP}/g" cpu/configs/*
+
+    # Cambiar IP de kernel en consola
+    perl -pi -e "s/(?<=IP_KERNEL=).*/${kernelIP}/g" consola/configs/*
+}
+
+main "$@"
